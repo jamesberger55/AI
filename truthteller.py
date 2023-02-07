a677a3be9757cb0d298e7472bdee611707246f57
James Berger <62909049+jamesberger55@users.noreply.github.com>
2023-02-07 13:36:18 -0600

    Update truthteller.py

--- truthteller.py
+++ truthteller.py
@@ -2,10 +2,10 @@ import SATSolver
 
 clauses = [[-1,3], [-2,-3], [2,3], [-1,2,-3], [1,3], [-2,3]]
 
-print('Knowledge base is satisfiable:',SATSolver.testKb(clauses))
-print('If we add Amy as a truth teller to the knowledge base: ', SATSolver.testLiteral(1,clauses))
-print('If we add Bob as a truth teller to the knowledge base: ', SATSolver.testLiteral(2,clauses))
-print('If we add Cal as a truth teller to the knowledge base: ', SATSolver.testLiteral(3,clauses))
+print('Is the Knowledge Base satisfiable? (True/False):',SATSolver.testKb(clauses))
+print('If we assume that Amy is a truth teller and add her to the knowledge base: ', SATSolver.testLiteral(1,clauses))
+print('If we assume that Bob is a truth teller and add him to the knowledge base: ', SATSolver.testLiteral(2,clauses))
+print('If we assume that Cal is a truth teller and add him to the knowledge base: ', SATSolver.testLiteral(3,clauses))
 
 clauses = [[-1,3], [-2,-3], [2,3], [-1,2,-3], [1,3], [-2,3], [3], [-2], [-1]]
-print('Thus, we have the Cal is a truth-teller, and Amy and Bob are liars. We get the following when adding Cal, not Amy and not Bob to knowledge base: ', SATSolver.testKb(clauses))
+print('Therefore, we have shown that Cal is a truth-teller, and Amy and Bob are liars. We got to this conclusion when we added Cal as a truth teller, and not Amy, and not Bob to knowledge base: ', SATSolver.testKb(clauses))
